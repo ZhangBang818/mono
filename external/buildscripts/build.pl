ac3638fc547d34d8f517ddfa23e0e9a9fbd7c17e
ashwini <ashwini@unity3d.com>
2018-11-28 14:59:02 -0500

    Chmod executables

--- external/buildscripts/build.pl
+++ external/buildscripts/build.pl
@@ -382,6 +382,7 @@ if ($build)
 		{
 			print(">>> Installing autoconf from $autoconfDir\n");
 			chdir("$autoconfDir") eq 1 or die ("failed to chdir to autoconf directory\n");
+			system("chmod", "-R", "755", $autoconfDir);
 			system("ls -al") eq 0 or die ("failed to list all files\n");
 			system("./configure --prefix=$builtToolsDir") eq 0 or die ("failed to configure autoconf\n");
 			system("make") eq 0 or die ("failed to make autoconf\n");
@@ -405,6 +406,7 @@ if ($build)
 		{
 			print(">>> Installing texinfo from $texinfoDir\n");
 			chdir($texinfoDir) eq 1 or die ("failed to chdir to texinfo directory\n");
+			system("chmod", "-R", "755", $texinfoDir);
 			system("./configure --prefix=$builtToolsDir") eq 0 or die ("failed to configure texinfo\n");
 			system("make") eq 0 or die ("failed to make texinfo\n");
 			system("make install") eq 0 or die ("failed to make install texinfo\n");
@@ -434,6 +436,7 @@ if ($build)
 				system("./bootstrap.sh") eq 0 or die ("failed to bootstrap automake\n");
 				$automakeMakeFlags = "-i";
 			}
+			system("chmod", "-R", "755", $automakeDir);
 			system("./configure --prefix=$builtToolsDir") eq 0 or die ("failed to configure automake\n");
 			system("make $automakeMakeFlags") eq 0 or die ("failed to make automake\n");
 			system("make install");
@@ -456,6 +459,7 @@ if ($build)
 		{
 			print(">>> Installing libtool from $libtoolDir\n");
 			chdir("$libtoolDir") eq 1 or die ("failed to chdir to libtool directory\n");
+			system("chmod", "-R", "755", $libtoolDir);
 			system("./configure --prefix=$builtToolsDir") eq 0 or die ("failed to configure libtool\n");
 			system("make") eq 0 or die ("failed to make libtool\n");
 			system("make install") eq 0 or die ("failed to make install libtool\n");
